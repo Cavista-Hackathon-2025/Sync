a9847a3aad8a04ac2d12620f90ecf3d364749c75
bukunmikuti <bukunmikuti@gmail.com>
2025-02-23 07:57:44 +0100

    change welcome sms alert

--- server/api/send-sms.js
+++ server/api/send-sms.js
@@ -5,14 +5,14 @@ export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
   const accountSid = config.TWILIO_ACCOUNT_SID;
   const authToken = config.TWILIO_AUTH_TOKEN;
-  
+
   const client = twilio(accountSid, authToken);
 
   try {
     const message = await client.messages.create({
-      body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+      body: "Welcome To BloodSync! Your willingness to donate makes a real difference. We'll alert you when it's time to give the gift of life.",
       from: "+13865171384",
-      to: "+2347062065345"
+      to: "+2347062065345",
     });
 
     // Return a response with the message body (or any relevant details)
